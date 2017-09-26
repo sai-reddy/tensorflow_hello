a5da274d717669e80a6aae38dc2a39b6b9b7b0d2
sai kalyan <saikalyar.p@gmail.com>
2017-09-26 15:36:49 -0400

    computing loss

--- hello.py
+++ hello.py
@@ -1,12 +1,29 @@
 import tensorflow as tf
 
-node1 = tf.placeholder(tf.float32)
-node2 = tf.placeholder(tf.float32)
+#initialize
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
 
-adder_node = node1 + node2
-add_and_triple = adder_node * 3
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
 
+# create model
+model = W * x + b
+
+# define loss
+squared_deltas = tf.square(model - y)
+loss = tf.reduce_sum(squared_deltas)
+
+# fix W and b
+fixW = tf.assign(W, [-1.])
+fixb = tf.assign(b, [1.])
+
+# initialize variables for session
+init = tf.global_variables_initializer()
 sess = tf.Session()
-print(sess.run(adder_node, {node1: 1, node2: 2}))
-print(sess.run(adder_node, {node1: [1, 2], node2: [1, 2]}))
-print(sess.run(add_and_triple, {node1: 1, node2: 2}))
+
+sess.run(init)
+sess.run([fixW, fixb])
+
+# run session
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
